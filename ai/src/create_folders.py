56bc6a374cf890031d4b0115d877cc968336ebc4
jeremygustine <jeremy@crafted.solutions>
2023-12-09 23:46:30 -0700

    Print GPU stats before training

--- ai/src/create_folders.py
+++ ai/src/create_folders.py
@@ -3,6 +3,29 @@ import torch
 print(f"Is cuda available? {torch.cuda.is_available()}")
 print(f"PyTorch version: {torch.__version__}")
 
+
+def print_gpu_memory():
+    if torch.cuda.is_available():
+        # Get the number of GPUs available
+        n_gpu = torch.cuda.device_count()
+        print(f"Number of GPUs available: {n_gpu}")
+
+        for i in range(n_gpu):
+            # Get the memory usage in bytes and convert to megabytes
+            total_memory = torch.cuda.get_device_properties(i).total_memory / 1e6
+            free_memory = torch.cuda.memory_allocated(i) / 1e6
+            used_memory = torch.cuda.memory_reserved(i) / 1e6
+
+            print(f"GPU {i}:")
+            print(f"  Total Memory: {total_memory:.2f} MB")
+            print(f"  Used Memory : {used_memory:.2f} MB")
+            print(f"  Free Memory : {free_memory:.2f} MB")
+    else:
+        print("No CUDA GPUs are available")
+
+
+print_gpu_memory()
+
 token_word = "ohwx"
 class_word = "dog"
 training_repeats = 40
