5132e474dd8d595265b5ba3683acd012d3c6402d
jeremygustine <jeremy@crafted.solutions>
2023-12-12 16:04:53 -0700

    Seed Holiday Packs 1, 2, and 3

--- app/core/management/commands/seedpacks.py
+++ app/core/management/commands/seedpacks.py
@@ -1,16 +1,26 @@
 import uuid
 from django.core.management.base import BaseCommand
 from core.models import PromptPack
+from .prompts import holiday_pack_1, holiday_pack_2, holiday_pack_3
 
 
 class Command(BaseCommand):
     help = 'Seeds the database with PromptPack data'
 
     def handle(self, *args, **kwargs):
-        holiday_prompts = ["prompt1", "prompt3"]
         PromptPack.objects.update_or_create(
-            name="Holiday",
-            defaults={'prompts': holiday_prompts}
+            name="Holiday Pack 1",
+            defaults={'prompts': holiday_pack_1}
+        )
+
+        PromptPack.objects.update_or_create(
+            name="Holiday Pack 2",
+            defaults={'prompts': holiday_pack_1}
+        )
+
+        PromptPack.objects.update_or_create(
+            name="Holiday Pack 3",
+            defaults={'prompts': holiday_pack_1}
         )
 
         self.stdout.write(self.style.SUCCESS('PromptPacks were seeded successfully.'))
