3774a1068813c05ab30ac9b8b9552ad1a720f786
jeremygustine <jeremy@crafted.solutions>
2023-12-20 00:02:03 -0700

    AI: Undo TE2 training, increase batch size to 2

--- ai/runpod/build_image_docker_hub.sh
+++ ai/runpod/build_image_docker_hub.sh
@@ -9,6 +9,6 @@ docker build \
   --build-arg AWS_S3_REGION_NAME="$AWS_S3_REGION_NAME" \
   -t sdxl:latest \
   -f Dockerfile .
-docker tag sdxl:latest jgustine/photo-pack-generation-image:27
-docker push jgustine/photo-pack-generation-image:27
+docker tag sdxl:latest jgustine/photo-pack-generation-image:28
+docker push jgustine/photo-pack-generation-image:28
 
