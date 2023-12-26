4380c0dc3f000bf5c4912d89d78bf0d26ec26ddf
Sarah Jerger <sarahjerger@sjergerGXW7X.vmware.com>
2023-12-26 17:02:50 -0600

    Hero section centered vertically

--- app/frontend/src/components/Sections/Hero.js
+++ app/frontend/src/components/Sections/Hero.js
@@ -3,35 +3,35 @@ import { STATIC_ROOT } from '../../constants'
 
 function Hero() {
   return (
-    <section className="bg-white dark:bg-gray-900 min-h-screen">
-      <div className="flex flex-col h-full my-auto px-0 py-4 md:px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 justify-center align-center">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-4xl font-extrabold tracking-tight leading-none text-primary-700 md:text-5xl lg:text-6xl dark:text-white tracking-tight">
-            {'Create Your Perfect'.toUpperCase()}
-          </h1>
-          <div className="flex justify-center">
-            <img
-              className=""
-              src={`${STATIC_ROOT}/hero_placeholder.png`}
-              alt="product image"
-            />
+      <section className="bg-white dark:bg-gray-900 min-h-screen flex items-center justify-center">
+          <div className="text-center">
+              <div className="flex flex-col gap-4">
+                  <h1 className="text-4xl font-extrabold tracking-tight leading-none text-primary-700 md:text-5xl lg:text-6xl dark:text-white tracking-tight">
+                      {'Create Your Perfect'.toUpperCase()}
+                  </h1>
+                  <div className="flex justify-center">
+                      <img
+                          className=""
+                          src={`${STATIC_ROOT}/hero_placeholder.png`}
+                          alt="product image"
+                      />
+                  </div>
+                  <h1 className="text-4xl font-extrabold tracking-tight leading-none text-primary-700 md:text-5xl lg:text-6xl dark:text-white tracking-tight">
+                      {'Profile Picture'.toUpperCase()}
+                  </h1>
+                  <p className="font-light text-gray-500 md:text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                      Upload your photos, get 100+ AI images.
+                  </p>
+              </div>
+              <div className="flex justify-center">
+                  <img
+                      className="pt-8 pb-2 rounded-t-lg"
+                      src={`${STATIC_ROOT}/icons/ScrollDown.svg`}
+                      alt="product image"
+                  />
+              </div>
           </div>
-          <h1 className="text-4xl font-extrabold tracking-tight leading-none text-primary-700 md:text-5xl lg:text-6xl dark:text-white tracking-tight">
-            {'Profile Picture'.toUpperCase()}
-          </h1>
-          <p className="font-light text-gray-500 md:text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-            Upload your photos, get 100+ AI images.
-          </p>
-        </div>
-        <div className="flex justify-center">
-          <img
-            className="pt-8 pb-2 rounded-t-lg"
-            src={`${STATIC_ROOT}/icons/ScrollDown.svg`}
-            alt="product image"
-          />
-        </div>
-      </div>
-    </section>
+      </section>
   )
 }
 
